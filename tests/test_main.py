4c918b3e70386417514746113fc2d21b4b798fcb
BD103 <bdeep10355@gmail.com>
2020-12-21 14:58:30 +0000

    Grid test

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,3 +17,15 @@ class Console(unittest.TestCase):
     assert bd103.console.load.loadbar(1, length=1) == "[##########]"
   def test_loadbar_overflow(self):
     assert bd103.console.load.loadbar(2, length=1) == "[##########]"
+  def test_grid_with_escape(self):
+    result = bd103.console.parser.grid("Hi there\nmy friend")
+    expected = ["Hi there", "my friend"]
+    assert result == expected
+  def test_grid_with_newline(self):
+    string = """
+Hello there.
+How are you?    
+"""
+    result = bd103.console.parser.grid(string.strip())
+    expected = ["Hello there.", "How are you?"]
+    assert result == expected
