83c8bd46e47b1b7871c9c9687b9475631d9e05a7
BD103 <59022059+BD103@users.noreply.github.com>
2021-11-21 18:42:50 +0000

    refactor: fix code with new tools

--- src/bd103/shared/funansi.py
+++ src/bd103/shared/funansi.py
@@ -3,38 +3,40 @@
 
 from random import randint
 
-
 _colors = {
-  "basic": [31, 33, 32, 36, 34, 35],
-  "bg": [41, 43, 42, 46, 44, 45],
-  "ex": [91, 93, 92, 96, 94, 95],
-  "bgex": [101, 103, 102, 106, 104, 105]
+    "basic": [31, 33, 32, 36, 34, 35],
+    "bg": [41, 43, 42, 46, 44, 45],
+    "ex": [91, 93, 92, 96, 94, 95],
+    "bgex": [101, 103, 102, 106, 104, 105],
 }
 
 
 def _rainbow_iter(palette="basic"):
-  while True:
-    for i in _colors[palette]:
-      yield i
+    while True:
+        for i in _colors[palette]:
+            yield i
 
 
 def make_rainbow(text: str, palette="basic") -> str:
-  res = ""
-  rotation = _rainbow_iter(palette)
+    res = ""
+    rotation = _rainbow_iter(palette)
 
-  for i in text:
-    res += f"\u001b[{next(rotation)}m{i}"
-  
-  rotation.close()
-  res += "\u001b[0m"
+    for i in text:
+        res += f"\u001b[{next(rotation)}m{i}"
 
-  return res
+    rotation.close()
+    res += "\u001b[0m"
+
+    return res
 
 
 def make_colorful(text: str, palette="basic"):
-  return f"\u001b[{_colors[palette][randint(0, len(_colors[palette]) - 1)]}m{text}\u001b[0m"
+    return "\u001b[{0}m{1}\u001b[0m".format(
+        _colors[palette][randint(0, len(_colors[palette]) - 1)],
+        text
+    )
 
 
 if __name__ == "__main__":
-  print(make_rainbow("This is some colorful text :)", "ex"))
-  print(make_colorful("This randomly is a different color"))
+    print(make_rainbow("This is some colorful text :)", "ex"))
+    print(make_colorful("This randomly is a different color"))
--- src/bd103/shared/hex.py
+++ src/bd103/shared/hex.py
@@ -1,51 +1,51 @@
 class Hex(object):
-  key = {
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "a": 10,
-    "b": 11,
-    "c": 12,
-    "d": 13,
-    "e": 14,
-    "f": 15
-  }
+    key = {
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "a": 10,
+        "b": 11,
+        "c": 12,
+        "d": 13,
+        "e": 14,
+        "f": 15,
+    }
 
-  def __init__(self, string: str):
-    self.og = Hex.make_safe(string)
+    def __init__(self, string: str):
+        self.og = Hex.make_safe(string)
 
-  def make_safe(string: str) -> str:
-    if string.startswith("#"):
-      string = string[1:]
-    
-    for i in string:
-      if i not in Hex.key:
-        raise Exception("Invalid Hex Code")
-    
-    return string.lower()
+    def make_safe(string: str) -> str:
+        if string.startswith("#"):
+            string = string[1:]
 
-  def convert_char(char: str) -> int:
-    return Hex.key.get(char)
+        for i in string:
+            if i not in Hex.key:
+                raise Exception("Invalid Hex Code")
 
-  def to_decimal(self) -> int:
-    sum = Hex.convert_char(self.og[0])
-    
-    if len(self.og) == 1:
-      return sum
-    
-    for i in self.og[1:]:
-      sum *= 16
-      sum += Hex.convert_char(i)
-    
-    return sum
+        return string.lower()
+
+    def convert_char(char: str) -> int:
+        return Hex.key.get(char)
+
+    def to_decimal(self) -> int:
+        sum = Hex.convert_char(self.og[0])
+
+        if len(self.og) == 1:
+            return sum
+
+        for i in self.og[1:]:
+            sum *= 16
+            sum += Hex.convert_char(i)
+
+        return sum
 
 
 if __name__ == "__main__":
-  print(Hex("ff").to_decimal())
+    print(Hex("ff").to_decimal())
