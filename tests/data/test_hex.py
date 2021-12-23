c1f62b156713c68ac85362f0eae1560fbcca94f0
BD103 <59022059+BD103@users.noreply.github.com>
2021-12-23 12:20:11 -0500

    chore: add some tests to hex module
    These are not all of the tests, just a few to lay down the foundation.

--- tests/data/test_hex.py
+++ tests/data/test_hex.py
@@ -1,15 +1,47 @@
 from bd103.data import hex
 
 
-class TestDumps(object):
-    def test_normal(self):
-        assert hex.dumps(0) == "00"
-        assert hex.dumps(15) == "0f"
-        assert hex.dumps(16) == "10"
-        assert hex.dumps(32) == "20"
-        assert hex.dumps(78) == "4e"
-        assert hex.dumps(255) == "ff"
+class TestHexEncoder(object):
+    def test_defaults(self):
+        encoder = hex.HexEncoder()
 
+        # Integer
+        assert encoder.encode_int(0) == "00"
+        assert encoder.encode_int(208) == "d0"
+        assert encoder.encode_int(255) == "ff"
 
-def test_dumps():
-    assert hex._default_encoder.encode_int(256) == "100"
+        # Bool
+        assert encoder.encode_bool(True) == "ff"
+        assert encoder.encode_bool(False) == "00"
+
+        # String
+        assert encoder.encode_str("{bd103}") == [
+            "ascii",
+            "7b",
+            "62",
+            "64",
+            "31",
+            "30",
+            "33",
+            "7d",
+        ]
+        assert encoder.encode_str("{bd103}", "utf-8") == [
+            "utf-8",
+            "7b",
+            "62",
+            "64",
+            "31",
+            "30",
+            "33",
+            "7d",
+        ]
+        assert encoder.encode_str("{bd103}", "utf-16") == [
+            "utf-16",
+            "fffe7b00",
+            "fffe6200",
+            "fffe6400",
+            "fffe3100",
+            "fffe3000",
+            "fffe3300",
+            "fffe7d00",
+        ]
